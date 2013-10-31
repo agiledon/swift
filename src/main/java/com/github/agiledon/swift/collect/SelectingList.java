a4cdb38c829be811cb8af258881fba951c7b8189
agiledon <bruce.e.zhang@gmail.com>
2013-10-31 11:09:53 +0800

    delegate all methods relate to selecting to SelectingList

---
+++ src/main/java/com/github/agiledon/swift/collect/SelectingList.java
@@ -0,0 +1,135 @@
+package com.github.agiledon.swift.collect;
+
+import com.github.agiledon.swift.base.Predicates;
+import com.github.agiledon.swift.exception.ElementNotFoundException;
+
+import java.util.List;
+
+final class SelectingList {
+
+    static <E> List<E> take(List<E> list, int length) {
+        List<E> result = SwiftList.newArrayList();
+        for (int i = 0; i < list.size(); i++) {
+            if (i < length) {
+                result.add(list.get(i));
+            }
+        }
+        return result;
+    }
+
+    static <E> List<E> takeRight(List<E> list, int rightLength) {
+        List<E> result = SwiftList.newArrayList();
+        int value = list.size() - rightLength;
+        int beginIndex = value >= 0 ? value : 0;
+        for (; beginIndex < list.size(); beginIndex++) {
+            result.add(list.get(beginIndex));
+        }
+        return result;
+    }
+
+    static <E> List<E> takeWhile(List<E> list, Predicates<E> predicate) {
+        List<E> result = SwiftList.newArrayList();
+        for (E element : list) {
+            if (predicate.apply(element)) {
+                break;
+            }
+            result.add(element);
+        }
+        return result;
+    }
+
+    static <E> List<E> drop(List<E> list, int length) {
+        List<E> result = SwiftList.newArrayList();
+        for (int i = 0; i < list.size(); i++) {
+            if (i >= length) {
+                result.add(list.get(i));
+            }
+        }
+        return result;
+    }
+
+    static <E> List<E> dropRight(List<E> list, int rightLength) {
+        List<E> result = SwiftList.newArrayList();
+        if (rightLength >= list.size()) {
+            return result;
+        }
+
+        for (int i = 0; i < list.size() - rightLength; i++) {
+            result.add(list.get(i));
+        }
+
+        return result;
+    }
+
+    static <E> List<E> dropWhile(List<E> list, Predicates<E> predicate) {
+        List<E> result = SwiftList.newArrayList();
+        boolean startFlag = false;
+        for (E element : list) {
+            if (predicate.apply(element)) {
+                startFlag = true;
+            }
+            if (startFlag) {
+                result.add(element);
+            }
+        }
+        return result;
+    }
+
+    static <E> E head(List<E> list) {
+        throwExceptionIfOneElement(list);
+        return list.get(0);
+    }
+
+    static <E> E last(List<E> list) {
+        throwExceptionIfOneElement(list);
+        return list.get(list.size() - 1);
+    }
+
+    private static <E> void throwExceptionIfOneElement(List<E> list) {
+        if (list.size() < 1) {
+            throw new ElementNotFoundException();
+        }
+    }
+
+    static <E> List<E> tail(List<E> list) {
+        if (list.size() < 2) {
+            return SwiftList.newArrayList();
+        }
+        List<E> result = SwiftList.newArrayList();
+        for (int i = 1; i < list.size(); i++) {
+            result.add(list.get(i));
+        }
+        return result;
+    }
+
+    static <E> List<E> init(List<E> list) {
+        if (list.size() < 2) {
+            return SwiftList.newArrayList();
+        }
+        List<E> result = SwiftList.newArrayList();
+        for (int i = 0; i < list.size() - 1; i++) {
+            result.add(list.get(i));
+        }
+        return result;
+    }
+
+    static <E> List<E> filter(List<E> filterFrom, Predicates<E> predicate) {
+        List<E> result = SwiftList.newArrayList();
+        for (E element : filterFrom) {
+            if (predicate.apply(element)) {
+                result.add(element);
+            }
+        }
+        return result;
+    }
+
+    static <E> List<E> filterNot(List<E> filterFrom, Predicates<E> predicate) {
+        List<E> result = SwiftList.newArrayList();
+        for (E element : filterFrom) {
+            if (!predicate.apply(element)) {
+                result.add(element);
+            }
+        }
+        return result;
+    }
+}
--- src/main/java/com/github/agiledon/swift/collect/SwiftList.java
+++ src/main/java/com/github/agiledon/swift/collect/SwiftList.java
@@ -2,7 +2,6 @@ package com.github.agiledon.swift.collect;
 
 import com.github.agiledon.swift.base.Functions;
 import com.github.agiledon.swift.base.Predicates;
-import com.github.agiledon.swift.exception.ElementNotFoundException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,71 +19,51 @@ public final class SwiftList {
     }
 
     public static <E> List<E> take(List<E> list, int length) {
-        List<E> result = newArrayList();
-        for (int i = 0; i < list.size(); i++) {
-            if (i < length) {
-                result.add(list.get(i));
-            }
-        }
-        return result;
+        return SelectingList.take(list, length);
     }
 
     public static <E> List<E> takeRight(List<E> list, int rightLength) {
-        List<E> result = newArrayList();
-        int value = list.size() - rightLength;
-        int beginIndex = value >= 0 ? value : 0;
-        for (; beginIndex < list.size(); beginIndex++) {
-            result.add(list.get(beginIndex));
-        }
-        return result;
+        return SelectingList.takeRight(list, rightLength);
     }
 
     public static <E> List<E> takeWhile(List<E> list, Predicates<E> predicate) {
-        List<E> result = newArrayList();
-        for (E element : list) {
-            if (predicate.apply(element)) {
-                break;
-            }
-            result.add(element);
-        }
-        return result;
+        return SelectingList.takeWhile(list, predicate);
     }
 
     public static <E> List<E> drop(List<E> list, int length) {
-        List<E> result = newArrayList();
-        for (int i = 0; i < list.size(); i++) {
-            if (i >= length) {
-                result.add(list.get(i));
-            }
-        }
-        return result;
+        return SelectingList.drop(list, length);
     }
 
     public static <E> List<E> dropRight(List<E> list, int rightLength) {
-        List<E> result = newArrayList();
-        if (rightLength >= list.size()) {
-            return result;
-        }
-
-        for (int i = 0; i < list.size() - rightLength; i++) {
-            result.add(list.get(i));
-        }
-
-        return result;
+        return SelectingList.dropRight(list, rightLength);
     }
 
     public static <E> List<E> dropWhile(List<E> list, Predicates<E> predicate) {
-        List<E> result = newArrayList();
-        boolean startFlag = false;
-        for (E element : list) {
-            if (predicate.apply(element)) {
-                startFlag = true;
-            }
-            if (startFlag) {
-                result.add(element);
-            }
-        }
-        return result;
+        return SelectingList.dropWhile(list, predicate);
+    }
+
+    public static <E> E head(List<E> list) {
+        return SelectingList.head(list);
+    }
+
+    public static <E> E last(List<E> list) {
+        return SelectingList.last(list);
+    }
+
+    public static <E> List<E> tail(List<E> list) {
+        return SelectingList.tail(list);
+    }
+
+    public static <E> List<E> init(List<E> list) {
+        return SelectingList.init(list);
+    }
+
+    public static <E> List<E> filter(List<E> filterFrom, Predicates<E> predicate) {
+        return SelectingList.filter(filterFrom, predicate);
+    }
+
+    public static <E> List<E> filterNot(List<E> filterFrom, Predicates<E> predicate) {
+        return SelectingList.filterNot(filterFrom, predicate);
     }
 
     public static <E> List<List<E>> split(List<E> partitionFrom, Predicates<? super E> predicates) {
@@ -105,58 +84,6 @@ public final class SwiftList {
         return result;
     }
 
-    public static <E> E head(List<E> list) {
-        throwExceptionIfOneElement(list);
-        return list.get(0);
-    }
-
-    public static <E> E last(List<E> list) {
-        throwExceptionIfOneElement(list);
-        return list.get(list.size() - 1);
-    }
-
-    public static <E> List<E> tail(List<E> list) {
-        if (list.size() < 2) {
-            return newArrayList();
-        }
-        List<E> result = newArrayList();
-        for (int i = 1; i < list.size(); i++) {
-            result.add(list.get(i));
-        }
-        return result;
-    }
-
-    public static <E> List<E> init(List<E> list) {
-        if (list.size() < 2) {
-            return newArrayList();
-        }
-        List<E> result = newArrayList();
-        for (int i = 0; i < list.size() - 1; i++) {
-            result.add(list.get(i));
-        }
-        return result;
-    }
-
-    public static <E> List<E> filter(List<E> filterFrom, Predicates<E> predicate) {
-        List<E> result = newArrayList();
-        for (E element : filterFrom) {
-            if (predicate.apply(element)) {
-                result.add(element);
-            }
-        }
-        return result;
-    }
-
-    public static <E> List<E> filterNot(List<E> filterFrom, Predicates<E> predicate) {
-        List<E> result = newArrayList();
-        for (E element : filterFrom) {
-            if (!predicate.apply(element)) {
-                result.add(element);
-            }
-        }
-        return result;
-    }
-
     public static <E> ArrayList<E> newArrayList() {
         return new ArrayList<E>();
     }
@@ -169,9 +96,4 @@ public final class SwiftList {
         return result;
     }
 
-    private static <E> void throwExceptionIfOneElement(List<E> list) {
-        if (list.size() < 1) {
-            throw new ElementNotFoundException();
-        }
-    }
 }
